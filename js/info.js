f9a7d70b52a48c24e2e78d5f2eff45013a972122
IceVSCode <davy.riato@gmail.com>
2021-09-05 15:24:18 +0200

    5497549745794579457

--- js/info.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  let year = (new Date()).getFullYear();
-  let age = document.getElementById("age");
-
-  if (age != undefined) {
-    age.innerHTML = year - 2005;
-  }
-});
